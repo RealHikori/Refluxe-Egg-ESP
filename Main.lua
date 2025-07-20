d07bd90f68f62727eb5438da78cce2c5ce4eb65e
RealHikori <reblexholder@gmail.com>
2025-07-21 01:59:21 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -244,7 +244,7 @@ autoBtn.TextSize = 16
 autoBtn.Font = Enum.Font.FredokaOne
 autoBtn.TextColor3 = Color3.new(1, 1, 1)
 
-local autoRunning = true
+local autoRunning = false
 local bestPets = {
     ["Raccoon"] = true, ["Dragonfly"] = true, ["Queen Bee"] = true,
     ["Disco Bee"] = true, ["Fennec Fox"] = true, ["Fox"] = true,
